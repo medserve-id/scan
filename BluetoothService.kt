e0cd26b06cd57f4afe85310c54ac1eb78dc2f319
medserve-id <mediaserver.idn@gmail.com>
2025-06-25 20:44:01 +0700

    Update BluetoothService.kt

--- BluetoothService.kt
+++ BluetoothService.kt
@@ -1,1 +1,22 @@
+class BluetoothService(private val context: Context) {
+
+    fun printText(text: String) {
+        val adapter = BluetoothAdapter.getDefaultAdapter()
+        val pairedDevices = adapter?.bondedDevices ?: return
+
+        val printer = pairedDevices.firstOrNull()  // Ambil printer pertama
+        val socket = printer
+            ?.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
+
+        socket?.connect()
+        val output = socket?.outputStream
+
+        // ESC/POS format
+        output?.write(text.toByteArray())
+        output?.write(byteArrayOf(0x0A)) // Line feed
+
+        output?.flush()
+        socket?.close()
+    }
+}
 
