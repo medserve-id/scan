4b67799163c3da1871f0355722663c5b0e65793e
medserve-id <mediaserver.idn@gmail.com>
2025-06-25 20:43:38 +0700

    Update Mainactivity.kt

--- Mainactivity.kt
+++ Mainactivity.kt
@@ -1,1 +1,28 @@
+class MainActivity : AppCompatActivity() {
+
+    lateinit var webView: WebView
+    lateinit var bluetoothService: BluetoothService
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bluetoothService = BluetoothService(this)
+
+        webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(JSBridge(), "AndroidBridge")
+        webView.loadUrl("file:///android_asset/index.html")
+
+        setContentView(webView)
+    }
+
+    inner class JSBridge {
+        @JavascriptInterface
+        fun onQrScanned(data: String) {
+            runOnUiThread {
+                bluetoothService.printText(data)
+            }
+        }
+    }
+}
 
